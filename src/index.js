8adc1e3d48b6342dd8235ff46dad6c97d11ead80
Mark Abramenko <mark.abramenko@epita.fr>
2025-03-14 10:50:55 +0100

    fix + feat(middleware): .gitignore accidentally empty and created middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const express = require("express")
 const app = express()
 port = 3000
 
+// MIDDLEWARE
+app.use((req, res, next) => {
+    const now = Date.now()
+    req.requestTime = now
+    next()
+   })
+   // data sent to the user
+   app.get("/", (req, res) => {
+    res.send(req.requestTime.toString())
+   })
+
 app.get("/", (req, res) => {
     res.send("Hewo")
 })
